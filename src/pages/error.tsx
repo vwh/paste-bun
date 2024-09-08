429ad4ccec5b0330919dea12698f139a7fa5d034
vwh <vwhe@proton.me>
2024-09-08 18:36:54 +0300

    feat: added zod validation and error page

---
+++ src/pages/error.tsx
@@ -0,0 +1,48 @@
+import { Html } from "@elysiajs/html";
+
+import AlertIcon from "@/components/icons/alert";
+import Head from "@/components/head";
+
+interface ErrorPageProps {
+  statsMessage: string;
+  errorMessage: string;
+}
+
+export default function ErrorPage({
+  statsMessage,
+  errorMessage,
+}: ErrorPageProps) {
+  return (
+    <html lang="en">
+      <head>
+        <Head />
+      </head>
+      <body class="bg-gray-800 text-gray-100 h-screen flex flex-col">
+        <nav class="bg-gray-800 p-3 flex items-center justify-between">
+          <a href="/" class="flex items-center gap-2">
+            <img
+              class="h-9"
+              src="./public/icons/logo.webp"
+              alt="Logo"
+              title="Logo"
+            />
+            <h1 class="text-xl font-bold">PasteBun</h1>
+          </a>
+        </nav>
+        <main class="flex-grow flex flex-col items-center justify-center">
+          <div class="flex flex-col text-center items-center">
+            <AlertIcon />
+            <h2 class="text-3xl font-bold mb-2 mt-2">{statsMessage}</h2>
+            <p class="text-gray-300 mb-6">{errorMessage}</p>
+            <a
+              href="/"
+              class="bg-gray-700 text-gray-200 py-2 px-4 rounded hover:bg-gray-600 transition-colors"
+            >
+              Go back home
+            </a>
+          </div>
+        </main>
+      </body>
+    </html>
+  );
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 import { Elysia, redirect, t } from "elysia";
 
+import { z } from "zod";
 import type { PasteManager } from "@/database";
 import authPlugin from "@/auth";
 
 import Home from "@/pages/home";
 import Paste from "@/pages/paste";
+import ErrorPage from "@/pages/error";
+
+// Zod Schemas
+export const Period = z.enum(["month", "week", "day", "hour"]);
+export const bodySchema = z.object({
+  content: z.string().max(5000, "Content must not exceed 5,000 characters."),
+  highlight: z.string().max(20, "Highlight must not exceed 20 characters."),
+  expiry: Period,
+});
 
 export const createRoutes = (pasteManager: PasteManager) => {
   return new Elysia()
@@ -12,15 +22,29 @@ export const createRoutes = (pasteManager: PasteManager) => {
     .get("/", () => Home())
     .post(
       "/",
-      ({ body, Auth: { ownerId } }) => {
-        const { content, expiry, highlight } = body;
-        const pasteId = pasteManager.insertPaste(
-          content,
-          highlight,
-          ownerId,
-          expiry
-        );
-        return redirect(`/${pasteId}`);
+      ({ set, error, body, Auth: { ownerId } }) => {
+        try {
+          const { content, expiry, highlight } = bodySchema.parse(body);
+          const pasteId = pasteManager.insertPaste(
+            content,
+            highlight,
+            ownerId,
+            expiry
+          );
+          return redirect(`/${pasteId}`);
+        } catch (err) {
+          if (err instanceof z.ZodError) {
+            const errorMessages = err.errors
+              .map((e) => `${e.path.join(".")}: ${e.message}`)
+              .join(", ");
+            set.status = 400;
+            return ErrorPage({
+              statsMessage: "400 - Bad Request",
+              errorMessage: errorMessages,
+            });
+          }
+          return error(500, "Internal server error");
+        }
       },
       {
         body: t.Object({
@@ -37,10 +61,15 @@ export const createRoutes = (pasteManager: PasteManager) => {
     )
     .get(
       "/:id",
-      ({ error, params: { id }, Auth: { ownerId } }) => {
+      ({ set, params: { id }, Auth: { ownerId } }) => {
         const paste = pasteManager.getPaste(id);
-        if (!paste) return error(404, "Paste not found.");
-
+        if (!paste) {
+          set.status = 404;
+          return ErrorPage({
+            statsMessage: "404 - Not Found",
+            errorMessage: "Paste not found.",
+          });
+        }
         const isOwner = paste.owner === ownerId;
         return Paste({ paste, isOwner });
       },
@@ -52,10 +81,15 @@ export const createRoutes = (pasteManager: PasteManager) => {
     )
     .get(
       "/raw/:id",
-      ({ error, params: { id } }) => {
+      ({ set, params: { id } }) => {
         const paste = pasteManager.getPaste(id);
-        if (!paste) return error(404, "Paste not found.");
-
+        if (!paste) {
+          set.status = 404;
+          return ErrorPage({
+            statsMessage: "404 - Not Found",
+            errorMessage: "Paste not found.",
+          });
+        }
         return new Response(paste.content, {
           headers: { "Content-Type": "text/plain" },
         });
@@ -68,12 +102,22 @@ export const createRoutes = (pasteManager: PasteManager) => {
     )
     .get(
       "delete/:id",
-      ({ error, params: { id }, Auth: { ownerId } }) => {
+      ({ set, params: { id }, Auth: { ownerId } }) => {
         const paste = pasteManager.getPaste(id);
-        if (!paste) return error(404, "Paste not found.");
-        if (paste.owner !== ownerId)
-          return error(403, "You are not the owner of this paste.");
-
+        if (!paste) {
+          set.status = 404;
+          return ErrorPage({
+            statsMessage: "404 - Not Found",
+            errorMessage: "Paste not found.",
+          });
+        }
+        if (paste.owner !== ownerId) {
+          set.status = 403;
+          return ErrorPage({
+            statsMessage: "403 - Forbidden",
+            errorMessage: "You do not have permission to delete this paste.",
+          });
+        }
         pasteManager.deletePaste(id);
         return redirect("/");
       },
