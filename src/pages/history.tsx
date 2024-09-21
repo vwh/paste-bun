469dcb5d50cd5be4b6f2b0ffc1739e4cf133683c
vwh <vwhe@proton.me>
2024-09-22 00:24:34 +0300

    ui: change some styles

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -53,58 +53,65 @@ export default function History({
         </nav>
         <div class="p-6">
           <h2 class="text-2xl font-bold mb-4">Your Pastes</h2>
-          <ul>
-            {pastes.map((paste) => (
-              <li key={paste.id} class="mb-4 bg-gray-900 p-4 rounded">
-                <div class="flex justify-between items-center">
-                  <span>
-                    {paste.content.slice(0, 30)}
-                    {paste.content.length > 30 ? "..." : ""}
-                  </span>
+          {pastes.length === 0 ? (
+            <p>No pastes found.</p>
+          ) : (
+            <>
+              {" "}
+              <ul>
+                {pastes.map((paste) => (
+                  <li class="mb-2 bg-gray-900 p-4 rounded">
+                    <div class="flex justify-between items-center">
+                      <span>
+                        {paste.content.slice(0, 30)}
+                        {paste.content.length > 30 ? "..." : ""}
+                      </span>
+                      <a
+                        href={`/edit/${paste.id}`}
+                        class="bg-gray-700 text-gray-200 p-2 rounded"
+                      >
+                        <PinIcon />
+                      </a>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <div class="flex justify-between mt-4">
+                {currentPage > 1 ? (
                   <a
-                    href={`/edit/${paste.id}`}
+                    href={`?page=${currentPage - 1}`}
                     class="bg-gray-700 text-gray-200 p-2 rounded"
                   >
-                    <PinIcon />
+                    Previous
                   </a>
-                </div>
-              </li>
-            ))}
-          </ul>
-
-          <div class="flex justify-between mt-4">
-            {currentPage > 1 ? (
-              <a
-                href={`?page=${currentPage - 1}`}
-                class="bg-gray-700 text-gray-200 p-2 rounded"
-              >
-                Previous
-              </a>
-            ) : (
-              <button
-                class="bg-gray-700 text-gray-400 p-2 rounded opacity-50 cursor-not-allowed"
-                disabled
-              >
-                Previous
-              </button>
-            )}
-
-            {currentPage < totalPages ? (
-              <a
-                href={`?page=${currentPage + 1}`}
-                class="bg-gray-700 text-gray-200 p-2 rounded"
-              >
-                Next
-              </a>
-            ) : (
-              <button
-                class="bg-gray-700 text-gray-400 p-2 rounded opacity-50 cursor-not-allowed"
-                disabled
-              >
-                Next
-              </button>
-            )}
-          </div>
+                ) : (
+                  <button
+                    type="button"
+                    class="bg-gray-700 text-gray-400 p-2 rounded opacity-50 cursor-not-allowed"
+                    disabled
+                  >
+                    Previous
+                  </button>
+                )}
+                {currentPage < totalPages ? (
+                  <a
+                    href={`?page=${currentPage + 1}`}
+                    class="bg-gray-700 text-gray-200 p-2 rounded"
+                  >
+                    Next
+                  </a>
+                ) : (
+                  <button
+                    type="button"
+                    class="bg-gray-700 text-gray-400 p-2 rounded opacity-50 cursor-not-allowed"
+                    disabled
+                  >
+                    Next
+                  </button>
+                )}
+              </div>
+            </>
+          )}
         </div>
       </body>
     </html>
